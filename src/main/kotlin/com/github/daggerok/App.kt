6b9bcba268ef30268021167209a87e49351b1d5b
Maksim Kostromin <daggerok@gmail.com>
2019-02-21 02:08:32 +0200

    Update App.kt

--- src/main/kotlin/com/github/daggerok/App.kt
+++ src/main/kotlin/com/github/daggerok/App.kt
@@ -4,6 +4,6 @@ const val groupId = "my-rocketmq-group"
 const val topicName = "my-rocketmq-topic"
 const val nameServerAddress = "127.0.0.1:9876"
 
-fun main(args: Array<String>) {
-    println("run command: gradle")
+fun main() {
+    println("see README.md")
 }
